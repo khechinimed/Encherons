2081bf0271d3bfb84de6fd2abf49cd83d82e49a7
Damien Borges <damien.borges777@gmail.com>
2022-11-09 15:53:27 +0100

    Inscription affichage erreur / confirmer le mot de passe

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -5,20 +5,27 @@ import { UserAuth } from '../context/AuthContext';
 const Signup = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [passwordConfirme, setPasswordConfirme] = useState('');
   const [error, setError] = useState('')
   const { createUser } = UserAuth();
   const navigate = useNavigate()
+  const [loading, setLoading] = useState(false);
 
   const handleSubmit = async (e) => {
-    e.preventDefault();
-    setError('');
-    try {
-      await createUser(email, password);
-      navigate('/home')
-    } catch (e) {
-      setError(e.message);
-      console.log(e.message);
+    if (password !== passwordConfirme) {
+      setError("Les mots de passe ne sont pas les mêmes");
+    }else{
+      e.preventDefault();
+      setError('');
+      try {
+        await createUser(email, password);
+        navigate('/home')
+      } catch (error) {
+        setError("La création de compte a échoué");
+        }
+        setLoading(false);
     }
+    
   };
 
   return (
@@ -30,7 +37,8 @@ const Signup = () => {
                   <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white text-center">
                       Inscription
                   </h1>
-                  <form onSubmit={handleSubmit} class="space-y-4 md:space-y-6" action="#">
+                  {error && <p class="text-center text-red-500">{error}</p>}
+                  <form onSubmit={handleSubmit} class="space-y-4 md:space-y-6">
                       <div>
                           <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Adresse email</label>
                           <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="nom@gmail.com" required="" onChange={(e) => setEmail(e.target.value)} />
@@ -38,6 +46,10 @@ const Signup = () => {
                       <div>
                           <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Mot de passe</label>
                           <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="" onChange={(e) => setPassword(e.target.value)} />
+                      </div>
+                      <div>
+                          <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirmer le mot de passe</label>
+                          <input type="password" name="passwordConf" id="passwordConf" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="" onChange={(e) => setPasswordConfirme(e.target.value)} />
                       </div>
                       <button type="submit" className="border border-blue-500 bg-blue-600 hover:bg-blue-500 w-full p-2 my-2 text-white rounded-lg">S'inscrire</button>
                       <p class="text-sm font-light text-gray-500 dark:text-gray-400 text-center">
