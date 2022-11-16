7ac5da0863e766145fded6429d8dd14cb2e561d5
khechinimed <khechinibakr20@gmail.com>
2022-11-16 23:39:18 +0100

    Create About and Contact components

---
+++ src/components/About.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function About() {
+    return (
+        <>
+            <div class="about-us">
+                <h1>À propos</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit, quis! Rem sed dignissimos in voluptatum qui molestias velit, cum laudantium consectetur odio voluptatem aut cupiditate assumenda id. Laborum, perspiciatis in.</p>
+            </div>
+        </>
+    )
+  }
+  
+  export default About;
